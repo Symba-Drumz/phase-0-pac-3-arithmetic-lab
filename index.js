3ddd7a2512162e4dafd1de928187a34d7fe3ed3f
alfazachmmosi <zalfa3890@gmail.com>
2024-03-20 06:36:32 +0300

    commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function add() {
-    // we'll fill this in shortly
-  }
+let number = 5;
+
+number++; //=> 5... hmmmm
+
+number; //=> 6 -- the number was incremented after it was evaluated
+
+number--; //=> 6
+
+number; //=> 5
